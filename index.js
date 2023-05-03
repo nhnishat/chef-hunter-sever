dab36dfb00a43c43ad41c086b1a71eec47ff72a7
nhnishat <nhnishat1998@gmail.com>
2023-05-04 02:17:51 +0600

    changes json

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
+app.use(cors());
+const chef = require('./Data/chefProfiles.json');
 
 app.get('/', (req, res) => {
 	res.send('hello world');
+});
+app.get('/chef', (req, res) => {
+	res.send(chef);
 });
 
 app.listen(port, () => {
